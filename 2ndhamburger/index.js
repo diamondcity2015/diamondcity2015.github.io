a460ffd22b21e9241087ea5421b60f34e88711af
diamondcity2015 <floridapanhandle5@hotmail.com>
2016-02-08 13:52:09 -0600

    2nd ham

--- 2ndhamburger/index.js
+++ 2ndhamburger/index.js
@@ -11,3 +11,11 @@
      })
    })
  </script>
+
+ <script type="text/javascript">
+ jQuery(function($){
+    	     $( '.menu-btn' ).click(function(){
+    	     $('.responsive-menu').toggleClass('expand')
+    	     })
+        })
+</script>
