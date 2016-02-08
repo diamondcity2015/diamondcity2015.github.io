3b1ed065ac5dceeb64f16b126da552b2b8026cab
diamondcity2015 <floridapanhandle5@hotmail.com>
2016-02-08 12:30:54 -0600

    cfg

--- cfg/index.js
+++ cfg/index.js
@@ -1,15 +1,3 @@
-// $( ".cross" ).hide();
-// $( ".menu" ).hide();
-// $( ".hamburger" ).click(function() {
-// $( ".menu" ).slideToggle( "slow", function() {
-// $( ".hamburger" ).hide();
-// $( ".cross" ).show();
-// });
-// });
-
-// $( ".cross" ).click(function() {
-// $( ".menu" ).slideToggle( "slow", function() {
-// $( ".cross" ).hide();
-// $( ".hamburger" ).show();
-// });
-// });
+$('button').click(function() {
+  $(this).toggleClass('expanded').siblings('div').slideToggle();
+});
