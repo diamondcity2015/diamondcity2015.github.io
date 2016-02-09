4557022d1c46872283704eba759711c556d049fb
diamondcity2015 <floridapanhandle5@hotmail.com>
2016-02-09 07:17:50 -0600

    9ham

--- 9ham/javascript/index.js
+++ 9ham/javascript/index.js
@@ -1,146 +1,61 @@
-var gulp 			= require( 'gulp' ),
-	sass 			= require( 'gulp-ruby-sass' ),
-	autoprefixer 	= require( 'gulp-autoprefixer' ),
-	minifycss 		= require( 'gulp-minify-css' ),
-	jshint 			= require( 'gulp-jshint' ),
-	uglify 			= require( 'gulp-uglify' ),
-	rename 			= require( 'gulp-rename' ),
-	concat 			= require( 'gulp-concat' ),
-	umd				= require( 'gulp-umd' );
+/**
+ * hamburger.js
+ *
+ * Mobile Menu Hamburger
+ * =====================
+ * A hamburger menu for mobile websites
+ *
+ * Created by Thomas Zinnbauer YMC AG  |  http://www.ymc.ch
+ * Date: 21.05.13
+ */
 
+jQuery(document).ready(function () {
 
-var outputDir 		= 'dist';
+    //Open the menu
+    jQuery("#hamburger").click(function () {
 
+        jQuery('#content').css('min-height', jQuery(window).height());
 
+        jQuery('nav').css('opacity', 1);
 
-//	Default task 'gulp': Runs both CSS and JS tasks
-gulp.task( 'default', function() {
-    gulp.start( 'css', 'js' );
+        //set the width of primary content container -> content should not scale while animating
+        var contentWidth = jQuery('#content').width();
+
+        //set the content with the width that it has originally
+        jQuery('#content').css('width', contentWidth);
+
+        //display a layer to disable clicking and scrolling on the content while menu is shown
+        jQuery('#contentLayer').css('display', 'block');
+
+        //disable all scrolling on mobile devices while menu is shown
+        jQuery('#container').bind('touchmove', function (e) {
+            e.preventDefault()
+        });
+
+        //set margin for the whole container with a jquery UI animation
+        jQuery("#container").animate({"marginLeft": ["70%", 'easeOutExpo']}, {
+            duration: 700
+        });
+
+    });
+
+    //close the menu
+    jQuery("#contentLayer").click(function () {
+
+        //enable all scrolling on mobile devices when menu is closed
+        jQuery('#container').unbind('touchmove');
+
+        //set margin for the whole container back to original state with a jquery UI animation
+        jQuery("#container").animate({"marginLeft": ["-1", 'easeOutExpo']}, {
+            duration: 700,
+            complete: function () {
+                jQuery('#content').css('width', 'auto');
+                jQuery('#contentLayer').css('display', 'none');
+                jQuery('nav').css('opacity', 0);
+                jQuery('#content').css('min-height', 'auto');
+
+            }
+        });
+    });
+
 });
-
-
-
-//	Watch task 'gulp watch': Starts a watch on CSS and JS tasks
-gulp.task( 'watch', function() {
-  gulp.watch( 'src/**/*.scss'	, [ 'css' ] );
-  gulp.watch( 'src/**/*.js'		, [ 'js' ] );
-});
-
-
-
-//	CSS task 'gulp css': Compiles all CSS
-gulp.task( 'css', [ 'css-concat-all' ] );
-
-//	1) compile all SCSS to CSS
-gulp.task( 'css-compile', function() {
-	return sass( 'src/**/*.scss', { style: 'expanded' })
-		.pipe( autoprefixer( 'last 2 version' ) )
-		.pipe( minifycss({ keepBreaks: true }) )
-		.pipe( gulp.dest( outputDir ) );
-});
-
-//	2) concatenate core + offCanvas in dist dir
-gulp.task( 'css-concat-core', [ 'css-compile' ], function() {
-	return gulp.src([
-			outputDir + '/css/jquery.mmenu.oncanvas.css',
-			outputDir + '/addons/offcanvas/jquery.mmenu.offcanvas.css',
-		])
-		.pipe( concat( 'jquery.mmenu.css' ) )
-		.pipe( gulp.dest( outputDir + '/css' ) );
-});
-
-//	3) concatenate core + offCanvas + addons in dist dir
-gulp.task( 'css-concat-all', [ 'css-concat-core' ], function() {
-	return gulp.src([
-			outputDir + '/css/jquery.mmenu.oncanvas.css',
-			outputDir + '/addons/offcanvas/jquery.mmenu.offcanvas.css',
-			outputDir + '/addons/**/*.css',
-			outputDir + '/extensions/**/*.css',
-			'!**/jquery.mmenu.iconbar.css',
-			'!**/jquery.mmenu.widescreen.css'
-		])
-		.pipe( concat( 'jquery.mmenu.all.css' ) )
-		.pipe( gulp.dest( outputDir + '/css' ) );
-});
-
-
-
-//	JS task 'gulp js': Runs all JS tasks
-//		A bit extensive, but it needs to concatenate certain files in a certain order
-//		The dependencies ensure everything is done in the right order
-gulp.task( 'js', [ 'js-umd' ] );
-
-//	1) copy all except for the navbars add-on into dist dir
-gulp.task( 'js-copy', function() {
-	return gulp.src([
-			'src/*/**/*.js',
-			'!src/addons/navbars/**/*.js'
-		])
-		.pipe( rename({ suffix: '.min' }) )
-		.pipe( gulp.dest( outputDir ) );
-});
-
-//	2) concatenate navbars add-on into dist dir
-gulp.task( 'js-concat-navbar', [ 'js-copy' ], function() {
-	return gulp.src([
-			'src/addons/navbars/jquery.mmenu.navbars.js',
-			'src/addons/navbars/**/*.js'
-		])
-		.pipe( concat( 'jquery.mmenu.navbars.min.js' ) )
-		.pipe( gulp.dest( outputDir + '/addons/navbars' ) );
-});
-
-//	3) concatenate core + offCanvas + scrollBugFix in dist dir
-gulp.task( 'js-concat-core', [ 'js-concat-navbar' ], function() {
-	return gulp.src([
-			outputDir + '/js/jquery.mmenu.oncanvas.min.js',
-			outputDir + '/addons/offcanvas/jquery.mmenu.offcanvas.min.js',
-			outputDir + '/addons/scrollbugfix/jquery.mmenu.scrollbugfix.min.js',
-		])
-		.pipe( concat( 'jquery.mmenu.min.js' ) )
-		.pipe( gulp.dest( outputDir + '/js' ) );
-});
-
-//	4) concatenate core + offCanvas + scrollBugFix + addons in dist dir
-gulp.task( 'js-concat-all', [ 'js-concat-core' ], function() {
-	return gulp.src([
-			outputDir + '/js/jquery.mmenu.oncanvas.min.js',
-			outputDir + '/addons/offcanvas/jquery.mmenu.offcanvas.min.js',
-			outputDir + '/addons/scrollbugfix/jquery.mmenu.scrollbugfix.min.js',
-			outputDir + '/addons/**/*.js'
-		])
-		.pipe( concat( 'jquery.mmenu.all.min.js' ) )
-		.pipe( gulp.dest( outputDir + '/js' ) );
-});
-
-//	5) minify all in dist dir
-gulp.task( 'js-minify', [ 'js-concat-all' ], function() {
-	return gulp.src([
-			outputDir + '/**/*.min.js'
-		])
-		.pipe( jshint('.jshintrc') )
-		.pipe( jshint.reporter( 'default' ) )
-		.pipe( uglify({ preserveComments: 'license' }) )
-		.pipe( gulp.dest( outputDir ) );
-});
-
-//	6) umd core + offCanvas + scrollBugFix + addons in dist dir
-gulp.task( 'js-umd', [ 'js-minify' ], function() {
-	return gulp.src([
-			outputDir + '/js/jquery.mmenu.min.js',
-			outputDir + '/js/jquery.mmenu.all.min.js',
-		])
-		.pipe( umd({
-			dependencies: function() { return [ 'jQuery' ]; },
-			exports: function() { return true; },
-			namespace: sanitizeNamespaceForUmd
-		}))
-		.pipe( rename({ suffix: '.umd' }) )
-		.pipe( gulp.dest( outputDir + '/js' ) );
-});
-function sanitizeNamespaceForUmd( file ) {
-	path = file.path.split( '\\' ).join( '/' ).split( '/' );
-	path = path[ path.length - 1 ];
-	return path.split( '.' ).join( '_' );
-}
-
