705b3bfcff6d4223a0d1611c007609aee5d1d98a
diamondcity2015 <floridapanhandle5@hotmail.com>
2016-03-16 17:34:09 -0500

    d

--- datepicker/js/main.js
+++ datepicker/js/main.js
@@ -1,3 +1,10 @@
 $(document).ready(function()    {
-
+	$("#birthday").datepicker({
+		changeMonth: true,
+		changeYear: true,
+		dateFormat: "MM dd, yy",
+		numberOfMonths: 1,
+		maxDate: 365,
+		minDate: -3650
+	});
 });
