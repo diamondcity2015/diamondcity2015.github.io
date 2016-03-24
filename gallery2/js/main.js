67a520389161f6259c142162e07ae2a6aa311d03
diamondcity2015 <floridapanhandle5@hotmail.com>
2016-03-24 15:10:56 -0500

    g2

--- gallery2/js/main.js
+++ gallery2/js/main.js
@@ -1,1 +1,7 @@
-<script type="text/javascript"> $(document).ready(function () { $('#divId img').on({ mouseover: function () { $(this).css({ 'cursor': 'hand', 'border-Color': 'red' }); }, mouseout: function () { $(this).css({ 'cursor': 'default', 'border-Color': 'grey' }); }, click: function () { var imageURL = $(this).attr('src'); $('#mainImage').fadeOut(500, function () { $(this).attr('src', imageURL); }).fadeIn(500); } }); }); </script>
+<script>
+ $(document).ready(function () { 
+ 	$('#divId img').on({ mouseover: function () { 
+ 		$(this).css({ 'cursor': 'hand', 'border-Color': 'red' }); },
+ 		 mouseout: function () { $(this).css({ 'cursor': 'default', 'border-Color': 'grey' }); }, 
+ 		 click: function () { var imageURL = $(this).attr('src'); $('#mainImage').fadeOut(500, function () { 
+ 		 	$(this).attr('src', imageURL); }).fadeIn(500); } }); }); </script>
