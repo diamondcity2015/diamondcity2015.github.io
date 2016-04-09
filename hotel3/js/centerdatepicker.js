717a43056e6c5c0c1e287584b572a4da0b2ea34d
diamondcity2015 <floridapanhandle5@hotmail.com>
2016-04-09 12:18:18 -0500

    h3

--- hotel3/js/centerdatepicker.js
+++
@@ -1,6 +0,0 @@
-$(document).on("click","#datepickerinputtag",function(){
-    var viewportwidth = $(window).width();
-    var datepickerwidth = $("#ui-datepicker-div").width();
-    var leftpos = (viewportwidth - datepickerwidth)/2; //Standard centering method
-    $("#ui-datepicker-div").css({left: leftpos,position:'absolute'});
-});
