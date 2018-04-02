e36d574087b74284b4d124a85f58242000718d48
diamondcity2015 <floridapanhandle5@hotmail.com>
2018-04-02 07:59:34 -0500

    sublime 4

---
+++ unit-7-lesson-4-master/js/main.js
@@ -0,0 +1,27 @@
+// Step 1: When the user clicks on the h1
+	// a) Fade toggle the img (which is hidden with css by default)
+$('h1').on('click',  Function()  {
+	$('img').fadeToggle();
+
+});
+
+// Step 2: When the user focuses on the input (the focus event)
+	// a) Add the active class to the input (which has been styled in the style.css)
+
+
+
+// Step 3: When the user clicks or tabs away from the input (the blur event)
+	// a) Remove the active class from the input
+
+
+
+// Step 4: Hide the item with the id #menu
+
+
+// Step 5: When the element with the id #dropdownButton is clicked,
+	// a) slide toggle #menu (look up the slideToggle method if you need a refresher)
+
+
+
+
+
