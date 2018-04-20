71a5bda488ec0043ff75afe94d297cb59c570d53
diamondcity2015 <floridapanhandle5@hotmail.com>
2018-04-20 17:42:05 -0500

    clock 2

--- clock2/js/main.js
+++ clock2/js/main.js
@@ -1,15 +1,115 @@
-$(document).ready(function() {
-        var now = new Date();
-        var hours = now.getHours();
-        var msg;
+//$(document).ready(function() {
+   //     var now = new Date();
+    //    var hours = now.getHours();
+    //    var msg;
 
-        if (hours < 12) msg = "Good Morning!";
-        else if (hours < 18) msg = "Good Afternoon!";
-        else msg = "Good Evening!";
-    $('#time').html('<p>'+msg+'</p>');
-    });
+     //   if (hours < 12) msg = "Good Morning!";
+     //   else if (hours < 18) msg = "Good Afternoon!";
+     //   else msg = "Good Evening!";
+    //$('#time').html('<p>'+msg+'</p>');
+    //});
+
+
+
+var days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+
+var today = new Date();
+var hours = today.getHours();
+
+var greeting
+
+// if day is sunday
+if (days === 0  && hours < 4) {
+	console.log('Open');
+}
+
+else if (days === 0  && hours > 4 && hours < 6) {
+	console.log('Closed');
+}
+
+else if (days === 0  && hours > 6) {
+	console.log('Open');
+}
+
+
+//if day is monday
+
+else if (days === 1 && hours < 1:31)  {
+	console.log('Open');
+}
+
+else if (days === 1 && hours > 1:30 && hours < 6)  {
+	console.log('Closed');
+}
+
+else if (days === 1 && hours > 6)  {
+	console.log('Open');
+}
+
+//if day is tuesday
+
+else if (days === 2 && hours < 1:31)  {
+	console.log('Open');
+}
+
+else if (days === 2 && hours > 1:30 && hours < 6)  {
+	console.log('Closed');
+}
+
+else if (days === 2 && hours > 6)  {
+	console.log('Open');
+}
+
+//if day is wednesday
+
+else if (days === 3 && hours < 1:31)  {
+	console.log('Open');
+}
+
+else if (days ===3 && hours > 1:30 && hours < 6)  {
+	console.log('Closed');
+}
+
+else if (days === 3 && hours > 6)  {
+	console.log('Open');
+}
+
+//if day is thursday
+
+else if (days === 4 && hours < 1:31)  {
+	console.log('Open');
+}
+
+else if (days ===4 && hours > 1:30 && hours < )  {
+	console.log('Closed');
+}
+
+else if (days === 4 && hours > 4)  {
+	console.log('Open');
+}
+
+//if day is friday
+else if (days === 5)   {
+	//greeting = 'Open';
+	console.log('Open');
+   }
+//if day is saturday
+else if (days === 6)   {
+	console.log('Open');
+	}
+
+document.write('<h3>' + greeting + '</h3>');
+
+
+
+
+
+
+
+
 
 //var today = new Date();
+//var hourNow = today.getHours();
 //var greeting
 
 //if (hourNow > 18) {
