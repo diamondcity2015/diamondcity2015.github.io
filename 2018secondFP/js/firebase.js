0f85e8fa80025c3330c6380b1e11ab19699ed65f
diamondcity2015 <floridapanhandle5@hotmail.com>
2018-04-23 09:45:03 -0500

    firebase

---
+++ 2018secondFP/js/firebase.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyBgXhwKPVhoN4g9FWKTaXf1Id2WbUEY2ng",
+    authDomain: "monkcafe-af7d1.firebaseapp.com",
+    databaseURL: "https://monkcafe-af7d1.firebaseio.com",
+    projectId: "monkcafe-af7d1",
+    storageBucket: "",
+    messagingSenderId: "185790520525"
+  };
+  firebase.initializeApp(config);
