69ed0a27bacc443b84b93c1bb6bfcbde2ca0f8b6
diamondcity2015 <floridapanhandle5@hotmail.com>
2018-04-24 16:44:38 -0500

    final

--- 2018secondFP/js/firebase.js
+++ 2018secondFP/js/firebase.js
@@ -18,7 +18,7 @@ var config = {
 
   var reservationData ={}
 
-  $(‘reservation-day li’).on('click', function () {
+  $(‘.reservation-day li’).on('click', function () {
   	reservationData.name = $('#name').val();
 	reservationData.date = $('#date').val();
 	});
@@ -32,44 +32,6 @@ var config = {
 
   function = getReservations ();
   database.ref('').push(reservationData);
-
-//class code
-
-// set the day when an option is clicked on
-$('.reservation-day li').click(function() {
- reservationData.day = $(this).text();
-});
-
-// when clicked, the name data should be set
-// and all data should be sent to your database
-$('.reservations').on('submit', function(event) {
-  // prevent reloading
-  event.preventDefault();
-
-  // get name from input
-  reservationData.name = $('.reservation-name').val();
-
-  // push configured data object to database
-  database.ref('reservations').push(reservationData);
-});
-
-
-// on initial load and addition of each reservation update the view
-database.ref('reservations').on('child_added', function(snapshot) {
-  // grab element to hook to
-  var reservationList = $('.reservation-list');
-  // get data from database
-  var reservations = snapshot.val();
-  // get your template from your script tag
-  var source   = $("#reservation-template").html();
-  // compile template
-  var template = Handlebars.compile(source);
-  // pass data to template to be evaluated within handlebars
-  // as the template is created
-  var reservationTemplate = template(reservations);
-  // append created templated
-  reservationList.append(reservationTemplate);
-});
 
 
 
