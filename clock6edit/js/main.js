6facf4f07c2fddfc8dc507a2a81aa97068958c62
diamondcity2015 <floridapanhandle5@hotmail.com>
2018-04-27 12:09:15 -0500

    c

--- clock6edit/js/main.js
+++ clock6edit/js/main.js
@@ -4,37 +4,87 @@ var currentDay = currentDate.getDay();
 var currentHour = currentDate.getHours();
 var currentMinute = currentDate.getMinutes();
 document.addEventListener('DOMContentLoaded', function() {
-  var friday = document.getElementById('friday');
-  var monThurs = document.getElementById("mon-thurs");
+  var sunday = document.getElementById('sunday');
+  var monWed = document.getElementById("mon-wed");
+  var thursday = document.getElementById('thursday');
+  var friSat = document.getElementById('fri-sat');
   var open = document.getElementById("open");
   var closed = document.getElementById("closed");
 
+  //var friday = document.getElementById('friday');
+  //var monThurs = document.getElementById("mon-thurs");
+  //var open = document.getElementById("open");
+  //var closed = document.getElementById("closed");
+
+  //function openClose() {
+    //reset display each time this function runs
+   // [monThurs, friday, open, closed].forEach(function(element) {
+   //   element.classList.remove('hidden');
+   // });
+
   function openClose() {
     //reset display each time this function runs
-    [monThurs, friday, open, closed].forEach(function(element) {
+    [sunday, monWed, thursday,friSat, open, closed].forEach(function(element) {
       element.classList.remove('hidden');
     });
+
+    //sunday open 0-4am and 6am-midnight
+
+    if (currentDay >= 0 && currentDay <= 1) {
+      monWed.classList.add('hidden');
+      thursday.classList.add('hidden');
+      friSat.classList.add('hidden');
+      
+      if (currentHour >= 0 && currentHour <= 4 && currentHour >= 6 && currentHour <= 24) {
+        closed.classList.add('hidden');
+      } else {
+        open.classList.add('hidden');
+      }
+      
+      //monday through wednesday  open 0-130am and 6am-midnight
+
+    }
     if (currentDay >= 1 && currentDay <= 4) {
-      friday.classList.add('hidden');
-      if (currentHour >= 8 && currentHour <= 21) {
+      sunday.classList.add('hidden');
+      thursday.classList.add('hidden');
+      friSat.classList.add('hidden');
+      if (currentHour >= 0 && currentHour <= 1.5  && currentHour >= 6 && currentHour <= 24) {
         closed.classList.add('hidden');
       } else {
         open.classList.add('hidden');
       }
     }
-    if (currentDay === 5) {
-      monThurs.classList.add('hidden');
-      if (currentHour >= 8 && currentHour <= 17.5) {
+
+    //thursday open 0-130am and 4am- midnight
+
+    if (currentDay >= 4 && currentDay <= 5) {
+      sunday.classList.add('hidden');
+      monWed.classList.add('hidden');
+      friSat.classList.add('hidden');
+      if (currentHour >= 0 && currentHour <= 1.5  && currentHour >= 4 && currentHour <= 24) {
         closed.classList.add('hidden');
       } else {
         open.classList.add('hidden');
       }
     }
-    if (currentDay === 0 || currentDay === 6) {
-      open.classList.add('hidden');
-      friday.classList.add('hidden');
+
+    // friday and saturday open 24 hours
+
+    if (currentDay >= 5) {
+      sunday.classList.add('hidden');
+      monWed.classList.add('hidden');
+      thursday.classList.add('hidden');
+      
+      closed.classList.add('hidden');
+      
     }
-  };
+
+
+//if (currentDay === 0 || currentDay === 6) {
+   //   open.classList.add('hidden');
+   //   friday.classList.add('hidden');
+   // }
+  //};
 
   setInterval(openClose, 5000);
   openClose();
---
+++ clock7/js/main.js
@@ -0,0 +1,42 @@
+var element = document.querySelectorAll('[data-target]')[0];
+var state = 'open';
+
+function changeState() {
+  state = 'open' === state ? 'closed' : 'open';
+  alert(state);
+}
+
+function runNext() {
+  var now = new Date();
+  var upcoming;
+  var nextChange;
+  var deltaDay = 0;
+  var deltaMS;
+
+  if ('open' === state) {
+    nextChange = element.getAttribute('data-close').split(':');
+  } else {
+    nextChange = element.getAttribute('data-open').split(':');
+    if (24 > now.getHours()) {
+      deltaDay = 1;
+    }
+  }
+
+  upcoming = new Date(
+    now.getFullYear(),
+    now.getMonth(),
+    now.getDate() + deltaDay,
+    nextChange[0],
+    nextChange[1],
+    0,
+    0
+  );
+
+  deltaMS =  upcoming - now;
+
+  setTimeout(function() {
+    changeState();
+  }, deltaMS); 
+}
+
+runNext();
