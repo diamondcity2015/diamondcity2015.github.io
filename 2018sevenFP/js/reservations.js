eae6315e8d51b2acdf4d131f5cbde35605ac4627
diamondcity2015 <floridapanhandle5@hotmail.com>
2018-04-28 14:47:33 -0500

    9

--- 2018sevenFP/js/reservations.js
+++ 2018sevenFP/js/reservations.js
@@ -1,7 +1,21 @@
 $('input').on('focus', function () {
+
   $('p').html('input is in focus!');
 });
+
 
 $('input').on('blur', function () {
   $('p').html('input is no longer in focus.');
 });
+
+$('button').on('submit', function (event) {
+	event.preventDefault();
+	if (input === [])
+  $('p').html('Please enter a name.');
+});
+
+$('button').on('click', function (event) {
+	event.preventDefault();
+	
+});
+
